89ca8181f84b9724ea5f25dc52f664a2d4dc47df
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-06-03 21:03:58 -0400

    typedefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,22 @@
+const typeDefs = `
+  type Query {
+    me: User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
