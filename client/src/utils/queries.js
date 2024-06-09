b8bd048bc45f2de224d79100b95f895adbbb8a58
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-06-08 23:07:45 -0400

    GET_ME queries FE

---
+++ client/src/utils/queries.js
@@ -0,0 +1,26 @@
+import {gql} from '@apollo/client';
+
+// Define the query
+
+// The query for getting the user's data
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+
+
